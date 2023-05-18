7fea190b5b0654b246c0d869b7e47cdd9ce93562
Tamilore <tamiloreolaniyi@gmail.com>
2023-05-18 14:47:07 +0100

    welcome screen

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons,SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Welcome</Text>
+    <View> 
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Tamilore</Text>
+        <Text style={styles.welcomeMessage}>Find your Perfect Job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput}/>
+        </View>
+      </View>
     </View>
   )
 }
